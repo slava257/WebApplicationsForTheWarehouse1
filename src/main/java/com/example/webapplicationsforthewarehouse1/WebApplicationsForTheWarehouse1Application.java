7565e4ddfd86b24e1addaddc849b0b2be92cb6a8
79650 <last137954682@gmail.ru>
2023-04-04 17:17:15 +0300

    Initial commit

---
+++ src/main/java/com/example/webapplicationsforthewarehouse1/WebApplicationsForTheWarehouse1Application.java
@@ -0,0 +1,13 @@
+package com.example.webapplicationsforthewarehouse1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApplicationsForTheWarehouse1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebApplicationsForTheWarehouse1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/webapplicationsforthewarehouse1/WebApplicationsForTheWarehouse1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webapplicationsforthewarehouse1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApplicationsForTheWarehouse1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
